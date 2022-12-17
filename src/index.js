e86b5e45ce88e1b59c534bf71215b6af28076dba
Miran-Mirantee <killerm11805@hotmail.com>
2022-12-17 23:58:51 +0700

    Implement diplaying a new todo list after adding a new todo

--- src/index.js
+++ src/index.js
@@ -20,12 +20,43 @@ project1.add(todo4);
 project1.add(todo5);
 project1.add(todo6);
 
-console.log(project1);
+project2.add(todo1);
+project2.add(todo2);
 
 // create a project displaying a list of todo
 const createProject = (project) => {
-    const list = document.createElement('div');
-    list.classList.add('project', 'container');
+    // create a list of todo
+    const _createListofTodo = () => {
+        list.classList.add('todo', 'list');
+        let length = project.list.length;
+        for (let i = 0; i < length; i++) {
+            const todoItem = document.createElement('div');
+            todoItem.classList.add('todo', 'container');
+    
+            const title = document.createElement('div');
+            title.classList.add('todo', 'title');
+            title.textContent = project.list[i].title;
+    
+            const desc = document.createElement('div');
+            desc.classList.add('todo', 'desc');
+            desc.textContent = project.list[i].desc;
+    
+            const dueDate = document.createElement('div');
+            dueDate.classList.add('todo', 'date');
+            dueDate.textContent = project.list[i].dueDate;
+    
+            const priority = document.createElement('div');
+            priority.classList.add('todo', 'priority');
+            priority.textContent = project.list[i].priority;
+    
+            todoItem.append(title, desc, dueDate, priority);
+            list.append(todoItem);
+            container.append(list);
+        }
+    };
+
+    const container = document.createElement('div');
+    container.classList.add('project', 'container');
 
     const topPanel = document.createElement('div');
     topPanel.classList.add('project', 'top-panel');
@@ -34,49 +65,32 @@ const createProject = (project) => {
     projectName.classList.add('project', 'name')
     projectName.textContent = project.name;
 
+    container.append(topPanel);
+
+    const list = document.createElement('div');
+    _createListofTodo();
+
     const addBtn = document.createElement('button');
     addBtn.classList.add('project', 'btn');
     addBtn.textContent = "add";
     addBtn.addEventListener('click', () => {
         createAddTodoModal(project);
+        const createTodoBtn = document.querySelector('.modal.btn:not(.cancel)');
+        createTodoBtn.addEventListener('click', () => {
+            list.innerHTML = '';
+            _createListofTodo();
+        });
     });
-
     topPanel.append(projectName, addBtn);
-    list.append(topPanel);
 
-    let length = project.list.length;
-
-    for (let i = 0; i < length; i++) {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo', 'container');
-
-        const title = document.createElement('div');
-        title.classList.add('todo', 'title');
-        title.textContent = project.list[i].title;
-
-        const desc = document.createElement('div');
-        desc.classList.add('todo', 'desc');
-        desc.textContent = project.list[i].desc;
-
-        const dueDate = document.createElement('div');
-        dueDate.classList.add('todo', 'date');
-        dueDate.textContent = project.list[i].dueDate;
-
-        const priority = document.createElement('div');
-        priority.classList.add('todo', 'priority');
-        priority.textContent = project.list[i].priority;
-
-        todoItem.append(title, desc, dueDate, priority);
-        list.append(todoItem);
-    }
-    document.body.append(list);
+    document.body.append(container);
 };
 
 // create a "add todo to project" modal
 const createAddTodoModal = (project) => {
-    
+
     // create a field container use in modal
-    const _fieldContainer = (form, type, labelTxt, name) => {
+    const _fieldContainer = (type, labelTxt, name) => {
         const container = document.createElement('div');
         container.classList.add('modal', 'input-container');
         const label = document.createElement('label');
@@ -85,8 +99,7 @@ const createAddTodoModal = (project) => {
         const field = document.createElement('input');
         setAttributes(field, {'type': type, 'id': name, 'name': name});
         container.append(label, field);
-        console.log(form);
-        form.append(container);
+        inputForm.append(container);
     };
 
     const modal = document.createElement('div');
@@ -105,19 +118,17 @@ const createAddTodoModal = (project) => {
     const closeBtn = document.createElement('span');
     closeBtn.classList.add('modal', 'close-btn');
     closeBtn.textContent = 'X';
-    closeBtn.addEventListener('click', () => {
-        modal.remove();
-    });
+    closeBtn.addEventListener('click', () => modal.remove());
 
     const inputForm = document.createElement('form');
     setAttributes(inputForm, {'action': 'javascript:;', 'method': 'post'});
     inputForm.classList.add('modal', 'input-form');
 
     // input fields
-    _fieldContainer(inputForm, 'text', 'Title', 'title');
-    _fieldContainer(inputForm, 'text', 'Description', 'desc');
-    _fieldContainer(inputForm, 'date', 'Due date', 'due_date');
-    _fieldContainer(inputForm, 'text', 'Priority', 'priority');
+    _fieldContainer('text', 'Title', 'title');
+    _fieldContainer('text', 'Description', 'desc');
+    _fieldContainer('date', 'Due date', 'due_date');
+    _fieldContainer('text', 'Priority', 'priority');
 
     const bottomPanel = document.createElement('div');
     bottomPanel.classList.add('modal', 'bottom-panel');
@@ -127,6 +138,14 @@ const createAddTodoModal = (project) => {
     createBtn.classList.add('modal', 'btn');
     createBtn.textContent = 'Create';
     createBtn.addEventListener('click', () => {
+        project.add(
+            new todo(
+                document.getElementById('title').value,
+                document.getElementById('desc').value,
+                document.getElementById('due_date').value,
+                document.getElementById('priority').value
+            )
+        );
         modal.remove();
     });
 
@@ -134,9 +153,7 @@ const createAddTodoModal = (project) => {
     setAttributes(cancelBtn, {'type': 'reset'});
     cancelBtn.classList.add('modal', 'btn', 'cancel');
     cancelBtn.textContent = 'Cancel';
-    cancelBtn.addEventListener('click', () => {
-        modal.remove();
-    });
+    cancelBtn.addEventListener('click', () => modal.remove());
     bottomPanel.append(cancelBtn, createBtn);
 
     inputForm.append(bottomPanel);
